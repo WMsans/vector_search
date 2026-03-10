47cf146e7d27a62381212c29665a4d90eb4df414
Jeremy <ggajy123@gmail.com>
2026-03-10 09:25:57 -0700

    fix: correct import paths and CSS order

--- frontend/src/components/search/index.js
+++ frontend/src/components/search/index.js
@@ -1,3 +1,3 @@
 export { default as ResultCard } from './ResultCard';
 export { default as ResultModal } from './ResultModal';
-export { default as ResultsList } from './ResultsList';
+export { default as ResultsList } from '../ResultsList';
