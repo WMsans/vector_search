8ee0596a41d3931b23855050fac2f58d3bf86163
Jeremy <ggajy123@gmail.com>
2026-03-22 21:37:16 -0700

    feat: use theme CSS variables in Layout

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar';
 
 export default function Layout({ children, appState, documentCount, lastIndexed, onReindex, onEditFolders }) {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen" style={{ backgroundColor: 'var(--theme-bg-1)' }}>
       <Sidebar
         appState={appState}
         documentCount={documentCount}
