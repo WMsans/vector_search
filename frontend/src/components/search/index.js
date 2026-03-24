b829f482ea2292eb521485a5b15839ee27cc44d6
Jeremy <ggajy123@gmail.com>
2026-03-23 20:01:14 -0700

    feat: export FileGroupCard and ChunkPreview components

--- frontend/src/components/search/index.js
+++ frontend/src/components/search/index.js
@@ -1,3 +1,5 @@
 export { default as ResultCard } from './ResultCard';
 export { default as ResultModal } from './ResultModal';
+export { default as FileGroupCard } from './FileGroupCard';
+export { default as ChunkPreview } from './ChunkPreview';
 export { default as ResultsList } from '../ResultsList';
